337ba1d468482077e51ed476b149ea962d4a4802
Kapil <kapilgangwar2003@gmail.com>
2025-02-21 23:38:11 +0530

    updated further

--- index.py
+++ index.py
@@ -2,9 +2,3 @@ from app import app
 
 # This is for Vercel serverless deployment
 handler = app
-
-def handler(request):
-    """
-    Vercel serverless function handler
-    """
-    return app(request) 
