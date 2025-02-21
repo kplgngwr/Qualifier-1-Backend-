ed73fc15d11d16753481c81440496e7bf82ea57c
Kapil <kapilgangwar2003@gmail.com>
2025-02-22 00:20:04 +0530

    updated further

--- index.py
+++
@@ -1,7 +0,0 @@
-# filepath: /c:/Users/Acer/Desktop/React Projects/Qualifier 1/my-backend-app/index.py
-from app import create_app
-
-app = create_app()
-
-# This is for Vercel serverless deployment
-handler = app
